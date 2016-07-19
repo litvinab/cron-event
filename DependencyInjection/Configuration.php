903f74c22d3554b2ca0f7c0b6e0b6dbb2e2cec6f
anton <anton.litvin@thepaydock.com>
2016-07-19 16:48:55 +0300

    DI prefix changed

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('litvinab_cron-event');
+        $treeBuilder->root('litvinab');
 
         return $treeBuilder;
     }
